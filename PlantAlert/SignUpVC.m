cc56211e599edd86b819c7225a2dda19b22db0bb
Casey White <casey.r.white@gmail.com>
2014-11-18 10:53:47 -0800

    Remove phone number field from sign up scene

--- PlantAlert/SignUpVC.m
+++ PlantAlert/SignUpVC.m
@@ -9,12 +9,11 @@
 #import "SignUpVC.h"
 #import "PANetworkingService.h"
 
-@interface SignUpVC () <UITextFieldDelegate>
+@interface SignUpVC ()
 
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
-@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTextField;
 
 @property (strong, nonatomic) PANetworkingService *apiService;
 
@@ -38,8 +37,6 @@
         UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:self action:@selector(navigateBack:)];
         self.navigationItem.leftBarButtonItem = backButton;
     }
-    
-    self.phoneNumberTextField.delegate = self;
 }
 
 - (IBAction)navigateBack:(id)sender {
@@ -47,8 +44,6 @@
 }
 
 - (IBAction)signUpPressed:(id)sender {
-    
-    NSLog(@"len %lu, num: %@", self.phoneNumberTextField.text.length, self.phoneNumberTextField.text);
 
     NSDictionary *userData = @{@"email"     : _emailTextField.text,
                                @"password"  : _passwordTextField.text};
@@ -60,67 +55,6 @@
             NSLog(@"Token: %@", token);
         }
     }];
-}
-
-#pragma mark - UITextFieldDelegate
-
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
-    
-    NSUInteger length = [self getLength:textField.text];
-    
-    if (length == 10) {
-        if (range.length == 0) {
-            return NO;
-        }
-    }
-    
-    if (length == 3) {
-        NSString *num = [self formatNumber:textField.text];
-        textField.text = [NSString stringWithFormat:@"(%@) ",num];
-        if(range.length > 0) {
-            textField.text = [NSString stringWithFormat:@"%@",[num substringToIndex:3]];
-        }
-    }
-    else if (length == 6) {
-        NSString *num = [self formatNumber:textField.text];
-        textField.text = [NSString stringWithFormat:@"(%@) %@-", [num substringToIndex:3], [num substringFromIndex:3]];
-        if (range.length > 0) {
-            textField.text = [NSString stringWithFormat:@"(%@) %@", [num substringToIndex:3], [num substringFromIndex:3]];
-        }
-    }
-    
-    return YES;
-}
-
-- (NSString *)formatNumber:(NSString *)mobileNumber {
-    
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@"(" withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@")" withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@" " withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@"-" withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@"+" withString:@""];
-    
-    NSLog(@"%@", mobileNumber);
-    
-    NSUInteger length = [mobileNumber length];
-    if (length > 10) {
-        mobileNumber = [mobileNumber substringFromIndex: length-10];
-        NSLog(@"%@", mobileNumber);
-    }
-    
-    return mobileNumber;
-}
-
-
-- (NSUInteger)getLength:(NSString *)mobileNumber {
-    
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@"(" withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@")" withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@" " withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@"-" withString:@""];
-    mobileNumber = [mobileNumber stringByReplacingOccurrencesOfString:@"+" withString:@""];
-    
-    return [mobileNumber length];
 }
 
 @end
